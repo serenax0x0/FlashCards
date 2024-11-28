a25696984bbb8d963ae5706566101dac7ca8c541
serenax0x0 <00001089756768SP@al.educacao.sp.gov.br>
2024-11-28 01:20:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Para que serve o Python?',
+    'O Python é usado para desenvolvimento web, ciência de dados, automação e mais'
+);
+
+criaCartao(
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'O maior país do mundo é a Rússia'
+);
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é um conjunto de passos para resolver um problema'
+);
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em Inglês?',
+    'Obrigado em inglês é Thank you (fénquiu)'
+);
